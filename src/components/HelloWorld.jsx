da208a7586797f01924f8c489c2b811ff0fc27a0
Pedro Pacheco <pedropacheco1523503@gmail.com>
2021-08-03 22:37:44 -0600

    eslint

--- src/components/HelloWorld.jsx
+++ src/components/HelloWorld.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import '../assets/styles/App.scss';
 
 const HelloWorld = () => {
-    return ( <h1>Hola Mundo!!</h1> );
-}
- 
+  return (<h1>Hola Mundo!!</h1>);
+};
+
 export default HelloWorld;
